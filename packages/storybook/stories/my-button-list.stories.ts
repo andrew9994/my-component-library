7eddd14f1af491acfed1e98814b356506ef37504
András Kaufmann <andraskaufmann@gmail.com>
2024-08-09 13:12:16 +0200

    docs: add story for my-button-list

---
+++ packages/storybook/stories/my-button-list.stories.ts
@@ -0,0 +1,11 @@
+export default {
+  title: 'My Button List',
+  component: 'my-button-list',
+};
+
+const Template = () =>
+  `
+  <my-button-list></my-button-list>
+`.trim();
+
+export const Default = Template.bind({});
